7d17968ecc609fb6fb4204516bc8d75759d6263f
Alastair McFarlane <alastair@play-consult.co.uk>
2022-12-09 15:22:31 +0000

    Fully dynamic day code

--- index.js
+++ index.js
@@ -38,50 +38,14 @@ if(!options.day) {
   winston.error("Specify which day to run with -d")
   error = true;
 } 
-// if(!options.input) {
-//   winston.error("Specify the input file with -i")
-//   error = true;
-// } 
 if(error) {
   process.exit(1);
 }
 
 async function main() {
-  let task;
-  switch(options.day) {
-    case(1):
-      task = require('./day1');
-      break;
-    case(2):
-      task = require('./day2');
-      break;
-    case(3):
-      task = require('./day3');
-      break;
-    case(4):
-      task = require('./day4');
-      break;
-    case(5):
-      task = require('./day5');
-      break;
-    case(6):
-      task = require('./day6');
-      break;
-    case(7):
-      task = require('./day7');
-      break;
-    case(8):
-      task = require('./day8');
-      break;
-    case(9):
-      task = require('./day9');
-      break;
-    case(10):
-      task = require('./day10');
-      break;
-  }
+  let task = require(`./day${options.day}`);
   winston.info(`Downloading input task for day ${options.day}`);
-  const input = download(options.day).then(input => {
+  download(options.day).then(input => {
     winston.info(`Starting task for day ${options.day}`);
     task(input)
     winston.info(`Finished`);
